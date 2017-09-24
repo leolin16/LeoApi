3846301613fb61f7534e536957df708a887377ca
Leo Lin <leolin@Leos-MacBook-Pro.local>
2017-09-24 22:01:47 +0800

    from-mac-1

--- Controllers/api/PointsOfInterestController.cs
+++ Controllers/api/PointsOfInterestController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LeoPortal2.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LeoPortal2.Controllers.api
@@ -19,7 +20,7 @@ namespace LeoPortal2.Controllers.api
             }
             return Ok(city.PointsOfInterest);
         }
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetPointOfInterest")]
         public IActionResult GetPointOfInterest(int cityId, int id)
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
@@ -33,6 +34,39 @@ namespace LeoPortal2.Controllers.api
                 return NotFound();
             }
             return Ok(PointOfInterest);
+        }
+                [HttpPost("")]
+        public IActionResult CreatePointOfInterest(int cityId, [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            if(pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+            if(ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if(pointOfInterest.Description == pointOfInterest.Name)
+            {
+                ModelState.AddModelError("Description", "The provided description should be different from the name.");
+            }
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+            return CreatedAtRoute("GetPointOfInterest", new {cityId = cityId, id = finalPointOfInterest.Id}, finalPointOfInterest);
         }
     }
 }
