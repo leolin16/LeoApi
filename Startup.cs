b81ffc541f3445433fc00ebf9c444d50bc09d379
leolin16 <leolin16@msn.com>
2017-11-01 20:43:01 +0800

    first?

--- Startup.cs
+++ Startup.cs
@@ -8,20 +8,15 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using LeoPortal2.Data;
 using LeoPortal2.Models;
-using LeoPortal2.Services;
 
 // just for writeAsync method
 using Microsoft.AspNetCore.Http;
 
 using Microsoft.AspNetCore.Mvc.Formatters;
 using AutoMapper;
-using LeoPortal2.Models.PageViewModels;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using System.Net;
-using LeoPortal2.Options;
-using LeoPortal2.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace LeoPortal2
@@ -35,11 +30,8 @@ namespace LeoPortal2
         private void ConfigureSettings(IServiceCollection services)
         {
             var configBuilder = new ConfigurationBuilder()
-                .SetBasePath(_env.ContentRootPath)
-                .AddJsonFile("alertThresholds.json");
-            var configFishTank = configBuilder.Build();
+                .SetBasePath(_env.ContentRootPath);
 
-            services.Configure<ThresholdOptions>(configFishTank);//need Microsoft.Extensions.Options.ConfigurationExtensions to make it pass the rule check
             services.AddOptions();
         }
     // Leo End
@@ -59,76 +51,6 @@ namespace LeoPortal2
             // services.AddDbContext<ApplicationDbContext>(options =>
             //     options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddDbContext<ApplicationDbContext>(
-                options =>
-                options.UseSqlServer(Configuration.GetConnectionString("SqlServerIdentityConnection"))
-                );
-
-            services.AddDbContext<WorldContext>(
-                //method changed to be implemented to WorldContext.cs (not yet determined)
-                options =>
-                options.UseSqlServer(Configuration.GetConnectionString("SqlServerDataConnection"))
-                );
-
-
-            services.AddIdentity<ApplicationUser, IdentityRole>(config =>
-            {
-                config.User.RequireUniqueEmail = true;
-                config.Password.RequiredLength = 8;
-                config.Password.RequireDigit = true;
-                config.Password.RequireUppercase = true;
-                config.Password.RequireLowercase = true;
-                config.Password.RequireNonAlphanumeric = true;
-                // config.Cookies.ApplicationCookie.LoginPath = "/Account/Login";
-                // config.Cookies.ApplicationCookie.LogoutPath = "/Account/LogOff";
-                //config.Cookies.ApplicationCookie.LoginPath = "/ASPNET/Account/Login";
-                //config.Cookies.ApplicationCookie.LogoutPath = "/ASPNET/Account/LogOff";
-                //config.Cookies.ApplicationCookie.LoginPath = "/Auth/Login";
-                //config.Cookies.ApplicationCookie.LogoutPath = "/Auth/Logout";
-                // config.Cookies.ApplicationCookie.Events = new CookieAuthenticationEvents()
-                // {
-                //     OnRedirectToLogin = ctx =>
-                //     {
-                //         //if (ctx.Request.Path.StartsWithSegments("/ASPNET/api") && ctx.Response.StatusCode == 200)
-                //         if (ctx.Request.Path.StartsWithSegments("/api") && ctx.Response.StatusCode == 200)
-                //         {
-                //             ctx.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-                //             return Task.FromResult<object>(null);
-                //         }
-                //         else
-                //         {
-                //             ctx.Response.Redirect(ctx.RedirectUri);
-                //             return Task.FromResult<object>(null);
-                //         }
-                //     }
-                // };
-
-            })
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
-
-            services.ConfigureApplicationCookie(options =>
-            {
-                options.LoginPath = "/Account/LogIn";
-                options.LogoutPath = "/Account/LogOut";
-                options.Events = new CookieAuthenticationEvents()
-                {
-                    OnRedirectToLogin = ctx =>
-                    {
-                        //if (ctx.Request.Path.StartsWithSegments("/ASPNET/api") && ctx.Response.StatusCode == 200)
-                        if (ctx.Request.Path.StartsWithSegments("/api") && ctx.Response.StatusCode == 200)
-                        {
-                            ctx.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-                            return Task.FromResult<object>(null);
-                        }
-                        else
-                        {
-                            ctx.Response.Redirect(ctx.RedirectUri);
-                            return Task.FromResult<object>(null);
-                        }
-                    }
-                };
-            });
 
             services.AddAutoMapper();
 
@@ -136,43 +58,21 @@ namespace LeoPortal2
             .AddMvcOptions(opt => opt.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter())); // determined from accept header of request
 
             // Add application services.
-            if(_env.IsEnvironment("Development") || _env.IsEnvironment("Testing"))
-            {
-                services.AddScoped<IMailService, DebugMailService>();
-            }
-            else
-            {
-                // Implement a real Mail Service
-                services.AddScoped<IMailService, DebugMailService>();
-            }
             // services.AddTransient<IEmailSender, EmailSender>();
-            services.AddTransient<CoordService>();
             services.AddLogging();
-            services.AddScoped<IWorldRepository, WorldRepository>();
-            services.AddTransient<AppContextSeedData>();
-            services.AddTransient<WorldContextSeedData>();
-            services.AddTransient<IEmailSender, AuthMessageSender>();
-            services.AddTransient<ISmsSender, AuthMessageSender>();
 
-            services.AddSingleton<ISensorDataService, SensorDataService>();
-            services.AddSingleton<IViewModelService, ViewModelService>();
             services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
-
-
-
-
-
 
             // Leo End
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, WorldContextSeedData seeder, AppContextSeedData seederApp)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
+                // app.UseDatabaseErrorPage();
             // Leo Start
                 app.UseBrowserLink();
             // Leo End
@@ -186,32 +86,16 @@ namespace LeoPortal2
 
             app.UseStaticFiles();
 
-            app.UseAuthentication();
-
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
 
-                routes.MapSpaFallbackRoute(
-                    name: "spa-fallback",
-                    defaults: new { controller = "Home", action = "Index" });
             });
 
-        // Leo Start
-            Mapper.Initialize(config =>
-            {
-                config.CreateMap<Trip, TripViewModel>().ReverseMap();
-                config.CreateMap<Stop, StopViewModel>().ReverseMap();
-                config.CreateMap<Favlink, FavlinkViewModel>().ReverseMap();
-                config.CreateMap<Link, LinkViewModel>().ReverseMap();
-                config.CreateMap<Cashflow, CashflowViewModel>().ReverseMap();
-                config.CreateMap<Cashitem, CashitemViewModel>().ReverseMap();
-            });
+            // Leo Start
 
-            seeder.EnsureSeedDataAsync().Wait();
-            seederApp.EnsureSeedDataAsync().Wait();
         // Leo End
 
             // blank project statement
